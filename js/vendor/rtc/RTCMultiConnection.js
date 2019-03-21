0e44b2009da4f41d1ad13bf6e0c0e090f20ae0c7
brangr <abrunenko@gmail.com>
2019-03-22 02:03:02 +0300

    stun/turn

--- js/vendor/rtc/RTCMultiConnection.js
+++ js/vendor/rtc/RTCMultiConnection.js
@@ -3431,28 +3431,45 @@ var RTCMultiConnection = function(roomid, forceOptions) {
         function getIceServers(connection) {
             // resiprocate: 3344+4433
             // pions: 7575
-            var iceServers = [{
-                    'urls': [
-                        'stun:webrtcweb.com:7788'
-                    ],
-                    'username': 'muazkh',
-                    'credential': 'muazkh'
+            // var iceServers = [{
+            //         'urls': [
+            //             'stun:webrtcweb.com:7788'
+            //         ],
+            //         'username': 'muazkh',
+            //         'credential': 'muazkh'
+            //     },
+            //     {
+            //         'urls': [
+            //             'turn:webrtcweb.com:7788', // coTURN 7788+8877
+            //             'turn:webrtcweb.com:8877',
+            //             'turn:webrtcweb.com:4455', // restund udp
+            //         ],
+            //         'username': 'muazkh',
+            //         'credential': 'muazkh'
+            //     },
+            //     {
+            //         'urls': [
+            //             'stun:stun.l.google.com:19302',
+            //             'stun:stun1.l.google.com:19302',
+            //             'stun:stun2.l.google.com:19302',
+            //             'stun:stun.l.google.com:19302?transport=udp',
+            //         ]
+            //     }
+            // ];
+
+	        var iceServers = [
+                {
+                    'urls':['turn:numb.viagenie.ca'],
+                    'username':'abrunenko@gmail.com',
+                    'credential':'bbvmY8RM'
                 },
                 {
                     'urls': [
-                        'turn:webrtcweb.com:7788', // coTURN 7788+8877
-                        'turn:webrtcweb.com:8877',
-                        'turn:webrtcweb.com:4455', // restund udp
-                    ],
-                    'username': 'muazkh',
-                    'credential': 'muazkh'
-                },
-                {
-                    'urls': [
-                        'stun:stun.l.google.com:19302',
-                        'stun:stun1.l.google.com:19302',
-                        'stun:stun2.l.google.com:19302',
-                        'stun:stun.l.google.com:19302?transport=udp',
+                        'stun:numb.viagenie.ca'
+                        //'stun:stun.l.google.com:19302',
+                        //'stun:stun1.l.google.com:19302',
+                        //'stun:stun2.l.google.com:19302',
+                        //'stun:stun.l.google.com:19302?transport=udp',
                     ]
                 }
             ];
