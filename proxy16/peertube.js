86879f248be85f4edd84ec22c71dadce00888a92
Alexander <alavrukhine@rixtrema.com>
2021-02-24 11:29:11 +0300

    error handling

--- proxy16/peertube.js
+++ proxy16/peertube.js
@@ -1,17 +1,52 @@
 const axios = require('axios');
 
+const STATS_METHOD = '/api/v1/videos';
+
 const Peertube = function () {
+  const hardCodeUrlsList = [
+    'pocketnetpeertube1.nohost.me',
+    'pocketnetpeertube2.nohost.me',
+    'pocketnetpeertube3.nohost.me',
+  ];
 
   this.destroy = () => {
-    return;
+    return Promise.resolve();
   };
 
   this.init = () => {
-    return;
+    return Promise.resolve();
   };
 
   this.kit = {
+    getRandomServer: () =>
+      Promise.resolve(
+        hardCodeUrlsList[Math.floor(Math.random() * hardCodeUrlsList.length)],
+      ),
 
+    getBestServer: () => {
+      const statsStack = hardCodeUrlsList.map((server) =>
+        axios
+          .get(`https://${server}${STATS_METHOD}`)
+          .then((res) => console.log(res.json())),
+      );
+
+      return axios.all(statsStack).then((res) => {
+        // console.log(res.map((respServ) => respServ.json()));
+
+        return Promise.resolve(res.json());
+      });
+    },
+
+    getTest: () =>
+      axios
+        .get(`https://132${hardCodeUrlsList[0]}${STATS_METHOD}`)
+        // .then((result) => result.json())
+        .then((result) => {
+          console.log('AAAA', result.data);
+
+          return Promise.resolve(result.data);
+        })
+        .catch((err) => Promise.reject(err)),
   };
 
   return this;
--- proxy16/proxy.js
+++ proxy16/proxy.js
@@ -1067,13 +1067,11 @@ var Proxy = function (settings, manage) {
         peertube : {
             servers : {
                 path : '/peertube/servers',
-                action : function(){
-                    return self.peertube.kit.get.history().then(d => {
-                        return Promise.resolve({
-                            data : d
-                        })
-                    })
-                }
+                action : () => self.peertube.kit.getTest().then(res => Promise.resolve({
+                    data: res,
+                })).catch(err => Promise.reject({
+                    data: err,
+                })),
             }
         },
 
