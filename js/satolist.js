206b2995e9175f10a727197a588c2dfcc31952ae
Alexander Dolgov <adolgov@rixtrema.com>
2021-03-13 09:30:23 +0300

    electron in satolist again

--- js/satolist.js
+++ js/satolist.js
@@ -17972,6 +17972,25 @@ Platform = function (app, listofnodes) {
                     $('#matrix').append(matrix);                 
                 }
 
+                var currentHref = self.app.nav.get.href();
+
+				var electronHrefs = JSON.parse(localStorage['electron_hrefs'] || "[]");
+			   
+				if (electronHrefs.indexOf(currentHref) == -1 && !electron){
+
+					electronHrefs.push(currentHref)
+
+					localStorage['electron_hrefs'] = JSON.stringify(electronHrefs.slice(electronHrefs.length - 100))
+
+					try{
+						window.location = 'pocketnet://electron/' + currentHref;
+					}
+					catch(e){
+						console.log("electron not installed")
+					}
+				   
+				} 
+
             } else {
                 self.app.user.features.telegram = 0;
             }
