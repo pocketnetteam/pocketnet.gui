2dc5c623f7bff84176d7ca6eeb00ca3567343dff
Slava V <letterthing@gmail.com>
2021-04-02 15:42:35 +0700

    webconfig adj

--- index.php
+++ index.php
@@ -34,18 +34,19 @@
             $og->echotags();
         ?>
 
-        <link rel="stylesheet" href="css/normalize.css?v=79596549113">
-<link rel="stylesheet" href="css/tooltipster.core.min.css?v=814684839408">
-<link rel="stylesheet" href="css/tooltipster.bundle.min.css?v=63307146011">
-<link rel="stylesheet" href="css/main.css?v=763953568024">
-<link rel="stylesheet" href="css/stblack.css?v=171755071902">
-<link rel="stylesheet" href="css/plyr.css?v=954655779551">
-<link rel="stylesheet" href="css/medium/medium-editor.css?v=694396070379">
-<link rel="stylesheet" href="css/medium/medium-editor-insert-plugin.css?v=674173702109">
-<link rel="stylesheet" href="js/vendor/DateTimePicker.min.css?v=977938163588">
-<link rel="stylesheet" href="css/medium/beagle.css?v=858343428671">
+        <link rel="stylesheet" href="css/normalize.css?v=161647921329">
+<link rel="stylesheet" href="css/tooltipster.core.min.css?v=413300566390">
+<link rel="stylesheet" href="css/tooltipster.bundle.min.css?v=437451742275">
+<link rel="stylesheet" href="css/main.css?v=261194142017">
+<link rel="stylesheet" href="css/stblack.css?v=916866753058">
+<link rel="stylesheet" href="css/plyr.css?v=38958863158">
+<link rel="stylesheet" href="css/medium/medium-editor.css?v=923467523954">
+<link rel="stylesheet" href="css/medium/medium-editor-insert-plugin.css?v=570951279123">
+<link rel="stylesheet" href="js/vendor/DateTimePicker.min.css?v=986078442158">
+<link rel="stylesheet" href="css/medium/beagle.css?v=123323135930">
 
-<link rel="icon" href="./favicon.svg" sizes="any" type="image/svg+xml">
+
+        <link rel="icon" href="./favicon.svg" sizes="any" type="image/svg+xml">
         <link rel="stylesheet" href="css/fontawesome/css/all.css">
 
         <script src="js/vendor/device.min.js?v=136"></script>
@@ -87,7 +88,6 @@
         <div class="chats">
         </div>
 
-        
 
         <div id="matrix" class="matrix"></div>
 
@@ -191,19 +191,19 @@
 <script join src="js/vendor/axios.js?v=7"></script>
 
         
-        <script>window.design = true;</script><script join src="js/functions.js?v=197134505266"></script>
-<script join src="js/user.js?v=596178472709"></script>
-<script join src="js/module.js?v=718403829729"></script>
-<script join src="js/navn.js?v=49910204341"></script>
-<script join src="js/validation.js?v=534965539286"></script>
-<script join src="js/_map.js?v=202731337317"></script>
-<script join src="js/localization.js?v=453231311904"></script>
-<script join src="js/kit.js?v=116424364120"></script>
-<script join src="js/satolist.js?v=33602759856"></script>
-<script join src="js/messenger2/clientrtc.js?v=750353078010"></script>
-<script join src="js/peertube-master.js?v=566875380267"></script>
-<script join src="js/app.js?v=178712902958"></script>
-<script join src="js/main.js?v=23564544897"></script>
+        <script>window.design = true;</script><script join src="js/functions.js?v=803450903299"></script>
+<script join src="js/user.js?v=192226667336"></script>
+<script join src="js/module.js?v=958529834623"></script>
+<script join src="js/navn.js?v=344090057657"></script>
+<script join src="js/validation.js?v=40964204368"></script>
+<script join src="js/_map.js?v=97661291909"></script>
+<script join src="js/localization.js?v=977721442102"></script>
+<script join src="js/kit.js?v=481666874150"></script>
+<script join src="js/satolist.js?v=24313274416"></script>
+<script join src="js/messenger2/clientrtc.js?v=633618577971"></script>
+<script join src="js/peertube-master.js?v=148395956466"></script>
+<script join src="js/app.js?v=70316622866"></script>
+<script join src="js/main.js?v=190064672070"></script>
 
 
 
