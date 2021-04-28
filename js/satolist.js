06c7debc71dfcff27a414d5d55ad7af6ad21deca
Alexander <alavrukhine@rixtrema.com>
2021-04-28 14:05:37 +0300

    refactor links function

--- js/satolist.js
+++ js/satolist.js
@@ -15161,33 +15161,26 @@ Platform = function (app, listofnodes) {
                     return self.sdk.videos.types.youtube(links)
                 },
 
-                peertube : function(links){
+                peertube : async function(links){
 
-                    console.log("links", links)
+                    console.log("links", links);
 
-                    var result = _.map(links, function(l){
+                    const linksInfo = await self.app.api.fetch('peertube/listVideos', {
+                        ids: links.map(link => link.link),
+                    });
 
-                        return self.app.api.fetch('peertube/video', {
-                            host : 'https://' + l.meta.host_name,
-                            id : l.meta.id
-                        }).then(d => {
-                            
+                    links.forEach(link => {
+                        const linkInfo = linksInfo[link.link];
+                        debugger;
+                        
+                        linkInfo ? link.d = {
+                            image : linkInfo.previewPath,
+                            views : linkInfo.views,
+                            duration : linkInfo.duration
+                        } : '';
+                    });
 
-                            l.data = {
-                                image : d.previewPath,
-                                views : d.views,
-                                duration : d.duration
-                            }
-
-                            return Promise.resolve(l)
-            
-                        }).catch(e => {
-                           return Promise.resolve(l)
-                        })
-
-                    })
-
-                    return Promise.all(result)
+                    return Promise.all(links);
 
                 },
 
