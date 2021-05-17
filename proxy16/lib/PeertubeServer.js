6529a6ace4a58ddbd127f27a5d69dad7b3a21dbb
Alexander <alavrukhine@rixtrema.com>
2021-05-17 13:14:56 +0300

    server stats update method

--- proxy16/lib/PeertubeServer.js
+++ proxy16/lib/PeertubeServer.js
@@ -1,3 +1,33 @@
-class PeertubeServer {};
+const { performance } = require('perf_hooks');
+
+const STATS_METHOD = '/api/v1/videos';
+
+class PeertubeServer {
+  constructor(url) {
+    this.url = url;
+  }
+
+  serverStats = [];
+
+  async getServerInfo() {
+    const { url, serverStats } = this;
+    const responseTime = performance.now();
+
+    await axios
+      .get(`https://${url}${STATS_METHOD}`)
+      .then((data) => {
+        responseTime = performance.now() - responseTime;
+
+        return data;
+      })
+      .then((res) => {
+        serverStats.push({
+          server: url,
+          total: res.value.data.total,
+          timeResponse: responseTime,
+        });
+      });
+  }
+}
 
 module.exports = PeertubeServer;
