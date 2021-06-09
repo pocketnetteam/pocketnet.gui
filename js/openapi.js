6a66bb63160e136d88a95d761014ed247c2d63b6
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2021-06-09 15:19:15 +0300

    Update openapi.js

--- js/openapi.js
+++ js/openapi.js
@@ -1,9 +1,10 @@
 
-if(typeof _Node == 'undefined') _Node = false;
-if(typeof _SEO == 'undefined') 	_SEO = false;
 
-if(!_Node)
-{
+
+retry(function(){
+	return typeof app != 'undefined'
+}, function(){
+
 	app = new Application({
 
 
@@ -13,8 +14,8 @@ if(!_Node)
 				host : 'pocketnet.app',
 				port : 8899,
 				wss : 8099
-		   },
-		   {
+			},
+			{
 				host : '1.pocketnet.app',
 				port : 8899,
 				wss : 8099
@@ -73,8 +74,5 @@ if(!_Node)
 			}
 		});
 	})
-	
 
-}
-
-topPreloader(100);
+})
