f91593bb875bd652cc2b7c7473f3873abe4dcde9
Alexander <alavrukhine@rixtrema.com>
2021-07-12 14:46:16 +0300

    add performance function on proxy

--- proxy16/peertube/roy.js
+++ proxy16/peertube/roy.js
@@ -1,144 +1,145 @@
-var _ = require('underscore')
+var _ = require('underscore');
 var f = require('../functions');
 const Instance = require('./instance');
 
-var Roy = function(parent){
-    var self = this
+var Roy = function (parent) {
+  var self = this;
 
-        self.parent = parent
+  self.parent = parent;
 
-    var instances = [];
-    var inited = false;
+  var instances = [];
+  var inited = false;
 
-    self.useall = false
+  self.useall = false;
 
-    self.addInstance = function(url){
-        var instance = new Instance(url, self)
+  self.addInstance = function (url) {
+    var instance = new Instance(url, self);
 
-        instance.init()
+    instance.init();
 
-        instances.push(instance)
+    instances.push(instance);
+  };
+
+  self.removeInstance = function (host) {
+    var instance = self.find(host);
+
+    if (instance) {
+      instance.destroy();
     }
 
-    self.removeInstance = function(host){
+    instances = _.filter(instances, function (instance) {
+      return instance.host == host;
+    });
+  };
 
-        var instance = self.find(host)
+  self.init = function (urls) {
+    _.each(urls, function (host) {
+      self.addInstance(host);
+    });
+  };
 
-        if (instance){
-            instance.destroy()
-        }
+  self.destroy = function () {
+    _.each(instances, function (instance) {
+      instance.destroy();
+    });
 
-        instances = _.filter(instances, function(instance){
-            return instance.host == host
-        })
+    instances = [];
 
+    inited = false;
+  };
+
+  self.bestlist = function () {
+    var _instances = _.filter(instances, function (instance) {
+      return instance.canuse() || self.useall;
+    });
+
+    return _.sortBy(_instances, function (instance) {
+      return -instance.stats().k;
+    });
+  };
+
+  self.best = function () {
+    var bestlist = self.bestlist();
+
+    if (bestlist.length) return bestlist[0];
+
+    return null;
+  };
+
+  self.requestToBest = function (method, data, p) {
+    var best = self.best();
+
+    if (!best) return Promise.reject('best');
+
+    return best.request(method, data, p);
+  };
+
+  self.direct = function (host, method, data, p) {
+    var instance = self.find(host);
+
+    if (!instance) {
+      return Promise.reject({
+        code: 404,
+        message: 'host',
+      });
     }
 
-    self.init = function(urls){
+    return instance.request(method, data, p);
+  };
 
-        _.each(urls, function(host){
+  self.request = function (method, data, p, list, index) {
+    if (!list) list = self.bestlist();
+    if (!index) index = 0;
 
-            self.addInstance(host)
-           
-        })
+    var instance = list[index];
 
-    }
+    if (!instance) return Promise.reject('failed');
 
-    self.destroy = function(){
-        _.each(instances, function(instance){
-            instance.destroy()
-        })
+    return instance
+      .request(method, data, p)
+      .catch((e) => {
+        if (e == 'failed')
+          return self.request(method, data, p, list, index + 1);
 
-        instances = []
+        return Promise.reject(e);
+      })
+      .then((r) => {
+        if (r.data) r.data.from = instance.host;
 
-        inited = false
-    }
+        return Promise.resolve(r);
+      });
+  };
 
-    self.bestlist = function(){
+  self.find = function (host) {
+    return _.find(instances, function (instance) {
+      return instance.host == host;
+    });
+  };
 
-        var _instances = _.filter(instances, function(instance){
-            return instance.canuse() || self.useall
-        })
+  self.info = function (compact) {
+    var info = {};
 
-        return _.sortBy(_instances, function(instance){
-            return -instance.stats().k
-        })
-    }
+    _.each(instances, function (instance) {
+      info[instance.host] = {
+        host: instance.host,
+        stats: instance.stats(),
+        canuse: instance.canuse(),
+      };
+    });
 
-    self.best = function(){
-        var bestlist = self.bestlist()
+    return info;
+  };
 
-        if (bestlist.length) return bestlist[0]
+  self.performance = () => {
+    const promises = instances.map((inst) => inst.stats());
 
-        return null
-    }
+    return Promise.all(promises).then((data) => {
+      console.log(data);
+      return data;
+    });
+  };
 
-    self.requestToBest = function(method, data, p){
-        var best = self.best()
-
-        if(!best) return Promise.reject('best')
-
-        return best.request(method, data, p)
-    }
-
-    self.direct = function(host, method, data, p){
-        var instance = self.find(host)
-
-        if(!instance){
-            return Promise.reject({
-                code : 404,
-                message : 'host'
-            })
-        }
-
-        return instance.request(method, data, p)
-    }
-
-    self.request = function(method, data, p, list, index){
-
-        if(!list) list = self.bestlist()
-        if(!index) index = 0
-
-        var instance = list[index]
-
-        if(!instance) return Promise.reject('failed')
-
-        return instance.request(method, data, p).catch(e => {
-
-            if (e == 'failed')  
-                return self.request(method, data, p, list, index + 1)
-
-            return Promise.reject(e)
-
-        }).then(r => {
-            if(r.data) r.data.from = instance.host
-
-            return Promise.resolve(r)
-        })
-    }
-    
-    self.find = function(host){
-        return _.find(instances, function(instance){
-            return instance.host == host
-        }) 
-    }
-
-    self.info = function(compact){
-        var info = {}
-
-        _.each(instances, function(instance){
-            info[instance.host] = {
-                host : instance.host,
-                stats : instance.stats(),
-                canuse : instance.canuse()
-            }
-        })
-
-        return info
-    }
-
-    return self
-}
-
+  return self;
+};
 
 module.exports = Roy;
