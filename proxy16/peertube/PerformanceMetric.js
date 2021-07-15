d65a9dc33dbbdef0b2dddde2b10346e4562d1f10
Alexander <alavrukhine@rixtrema.com>
2021-07-15 13:21:39 +0300

    new metrics structure

---
+++ proxy16/peertube/PerformanceMetric.js
@@ -0,0 +1,30 @@
+class PerformanceMetric {
+  _ratings = [];
+
+  calculate(instance) {
+    const { _ratings } = this;
+    const performance = instance.performance();
+
+    if (!performance) return -10;
+
+    const serverData = performance.data;
+
+    if (!serverData.performance) return -10;
+
+    const calculatedRatings = _ratings.map((rating) => rating(serverData));
+
+    return calculatedRatings.reduce((accumulator, metric) => {
+      const currentRating = metric.calculate() || 0;
+
+      return accumulator + currentRating;
+    }, 0);
+  }
+
+  constructor(ratings = [], customCalculator) {
+    this._ratings = ratings;
+
+    if (customCalculator) this._calculate = customCalculator;
+  }
+}
+
+module.exports = PerformanceMetric;
