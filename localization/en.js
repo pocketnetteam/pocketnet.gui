772ed6b1b30c39ee1617dc7ffdac7b1cfbee227d
Alexander <alavrukhine@rixtrema.com>
2021-07-21 13:49:08 +0300

    caption change

--- localization/en.js
+++ localization/en.js
@@ -1003,7 +1003,7 @@ ____loclib.period182day = "Half of year";
 ____loclib.shareBareLink = "Share Video Link";
 ____loclib.videoCopied = "Video link successfully copied to clipboard";
 
-____loclib.editWallpaper = "Change Wallpaper";
+____loclib.editWallpaper = "Change Preview Image";
 ____loclib.removeVideo = "Remove Video";
 
 ____loclib.removeVideoDialog = 'Are you sure you want to delete this video?'
