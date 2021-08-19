7b9b4283dfc80e8d9692a6afd227b84655586aae
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2021-08-19 10:59:52 +0300

    c

--- peertube/video-embed.bundle.js
+++ peertube/video-embed.bundle.js
@@ -43,7 +43,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + ".chunk.js?v=3954"
+/******/ 		return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + ".chunk.js?v=467"
 /******/ 	}
 /******/
 /******/ 	// The require function
