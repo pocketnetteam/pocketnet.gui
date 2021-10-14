360bf6d812efecebdc51501ad44801b94f64c9e7
Alexander Dolgov <adolgov@rixtrema.com>
2021-10-14 17:03:10 +0300

    author.js reverted

--- components/author/index.js
+++ components/author/index.js
@@ -1037,20 +1037,19 @@ var author = (function(){
 
 		var initEvents = function(){
 
+			var user = self.app.user
+
+			var my = (user.address.value && author.address == user.address.value)
 
 			var src = deep(author, 'data.image')
 
-			var me = self.app.platform.sdk.address.pnet() ? self.app.platform.sdk.address.pnet().address : null;
-			console.log('me', me);
-
-			if (!src && me === author.address){
+			if (!src && my){
 
 
-				el.usericon.addClass('active')
-
+				/*el.usericon.addClass('active')
 				self.app.platform.api.plissing({
 					el : el.usericon,
-				})
+				})*/
 	
 				el.usericon.on('click', function(){
 					self.app.nav.api.load({
@@ -1069,6 +1068,18 @@ var author = (function(){
 			el.subscribe.find('.subscribe').on('click', events.subscribe)
 			el.subscribe.find('.unsubscribe').on('click', events.unsubscribe)
 			el.c.find('.notificationturn').on('click', events.subscribePrivate)
+
+			el.c.find('.changeaccount').on('click', function(){
+				self.nav.api.go({
+					open : true,
+					href : 'accounts',
+					inWnd : true,
+
+					essenseData : {
+						toaccpage : true
+					}
+				})
+			})
 
 			el.caption.find('.startchat').on('click', events.startchat)
 
