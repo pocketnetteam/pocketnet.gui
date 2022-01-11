8eedcf27518a825ecb79c933f785675349c531f5
j-bitmaker <adolgov@rixtrema.com>
2022-01-11 17:08:25 +0300

    component created

--- js/_map.js
+++ js/_map.js
@@ -193,6 +193,33 @@ __map =  {
 		anonimus : true,
 	},
 
+	easynode : {
+		uri : "easynode",
+		href : "easynode",
+		add : function(settings, p){
+
+			if(p.inWnd)
+
+				return {
+					insert : 'wnd'
+				}
+
+			else
+			{
+				return {
+					el : 'content'
+				}
+			}
+
+		},
+		anonimus : true,
+		/*redirect : {
+			auth : 'authorization',
+			//validate : 'filluser'
+		},*/
+	
+	},
+
 	aboutHome : {
 		uri : "aboutHome",
 		href : "aboutHome",
