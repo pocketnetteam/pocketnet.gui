dc58af12dcc5c6c01d498cb42979416a7a003412
Alexander <alavrukhine@rixtrema.com>
2022-04-22 17:31:32 +0300

    optional new auth route for test

--- js/peertube.js
+++ js/peertube.js
@@ -159,7 +159,7 @@ PeerTubePocketnet = function (app) {
 		},
 
 		pocketnetAuth: {
-			path: 'api/v1/users/blockChainAuth',
+			path: app.test ? 'api/v1/users/blockChainAuth' : 'plugins/pocketnet-auth/router/code-cb',
 			signature: true,
 			method: 'POST',
 			axios: true,
