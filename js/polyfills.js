499ce816208727456ba700e859c7279bf26537b5
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-04-25 15:39:09 +0300

    Update polyfills.js

--- js/polyfills.js
+++ js/polyfills.js
@@ -896,3 +896,19 @@ if (!('remove' in Element.prototype)) {
       }
   };
 }
+
+
+(function() {
+	if (typeof globalThis === 'object') return;
+	Object.defineProperty(Object.prototype, '__magic__', {
+		get: function() {
+			return this;
+		},
+		configurable: true // This makes it possible to `delete` the getter later.
+	});
+	__magic__.globalThis = __magic__; // lolwat
+	delete Object.prototype.__magic__;
+}());
+
+// Your code can use `globalThis` now.
+console.log(globalThis);
