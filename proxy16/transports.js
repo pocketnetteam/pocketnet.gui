47456fb3e3043ceff53dff8d249794cd01f3bdfe
Alexandr Atchanov <cenitelas1@gmail.com>
2022-05-03 05:04:26 +0600

    Added Tor proxy binary

--- proxy16/transports.js
+++ proxy16/transports.js
@@ -2,12 +2,52 @@
 
 const _request = require("request");
 const _axios = require("axios");
+const path = require("path");
+const fs = require("fs");
 const { SocksProxyAgent } = require('socks-proxy-agent')
+const child_process = require("child_process");
+const {Notification} = require("electron");
 const httpsAgent = new SocksProxyAgent('socks5://127.0.0.1:9050')
+
+const getPathTor = ()=>{
+    const  isDevelopment = process.argv.some(function(el) { return el === '--development'; })
+    let pathTor = "";
+    switch (process.platform){
+        case 'win32':
+            pathTor = "tor/win/tor.exe"
+            break;
+        case 'linux':
+            pathTor = "tor/linux/tor"
+            break;
+        case 'darwin':
+            if(process.arch === "arm64") {
+                pathTor = "tor/m1/tor"
+            }else {
+                pathTor = "tor/osx/tor"
+            }
+            pathTor = (isDevelopment ? "" : "../") + pathTor;
+            break;
+    }
+    return path.join(isDevelopment ? __dirname : "", pathTor)
+}
 
 module.exports = function (enable){
     const self = {};
+    self.tor = {};
+   
+    self.tor.path = getPathTor();
+    self.runTor = async (eventCallBack)=>{
+        const log = async (data)=>{
+            eventCallBack?.(data)
+        }
+        self.tor.instance = child_process.spawn(self.tor.path, [], { stdio: ['ignore'], detached : false, shell : false})
+        self.tor.instance.on("error", (err)=>log(err));
+        self.tor.instance.on("exit", (code) => log(code));
+        self.tor.instance.stderr.on("data", (chunk) => log(String(chunk)));
+        self.tor.instance.stdout.on("data", (chunk) => log(String(chunk)));
+    }
     
+
     const axiosRequest =  async (method, ...args)=> {
         return enable ? await _axios[method]?.(...args) : await _axios[method]?.({...{httpsAgent: httpsAgent}, ...args})
     }
