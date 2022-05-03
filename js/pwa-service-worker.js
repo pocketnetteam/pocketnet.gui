acaa10e55dfee9778f4653ce824ed5f519ff7209
Alexandr Atchanov <cenitelas1@gmail.com>
2022-05-04 03:24:46 +0600

    added proxy plugin in workbox

--- js/pwa-service-worker.js
+++ js/pwa-service-worker.js
@@ -1,3 +1,6 @@
+const {Blob} = require("buffer");
+electron = require('electron');
+
 if ('serviceWorker' in navigator) {
     // Register a service worker hosted at the root of the site using the default scope
     navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
@@ -5,6 +8,18 @@ if ('serviceWorker' in navigator) {
     }, /*catch*/ function(error) {
         console.log('Service worker registration failed:', error);
     });
+
+    navigator.serviceWorker.addEventListener('message', async event => {
+        const buffer = await electron.ipcRenderer.invoke('proxyUrl', event.data || null);
+        const channel = new BroadcastChannel(event.data);
+        const res = await fetch(`data:${buffer.type};base64,${buffer.data}`);
+        const blob = await res.blob();
+        const url = URL.createObjectURL(blob)
+        channel.postMessage(url)
+    });
+    
+ 
 } else {
     console.log('Service workers are not supported.');
 }
+
