5375685c5fc166f04a243e61a67ced9cc04156c4
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-05-11 09:41:54 +0300

    v

--- js/satolist.js
+++ js/satolist.js
@@ -16810,13 +16810,7 @@ Platform = function (app, listofnodes) {
                         method : 'getrecommendedcontentbyaddress',
                         rpc : {
                             cache : true,
-                            locally : true,
-                            fastvideo : true,
-                            meta : {
-                                host : '78.37.233.202',
-                                port : 31031,
-                                ws : 3037
-                            }
+                            fastvideo : true
                         }
                     });
 
