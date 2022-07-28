ed8545bc888779c0eb7486be3833913da0298693
j-bitmaker <adolgov@rixtrema.com>
2022-07-28 15:49:20 +0300

    PKOIN comment error; total & available balance

--- js/satolist.js
+++ js/satolist.js
@@ -19021,7 +19021,7 @@ Platform = function (app, listofnodes) {
                                     //sitemessage(self.app.localization.e('e13117'))
 
                                     if (clbk){
-                                        clbk(null, self.app.localization.e('e13117'));
+                                        clbk(null, self.app.localization.e('incoins'));
                                     }
 
                                     return;
@@ -19326,7 +19326,7 @@ Platform = function (app, listofnodes) {
                                     if (obj.donate && obj.donate.v.length){
 
                                         obj.donate.v.forEach(function(d){
-                                            var donate = Number(d.amount) * smulti;
+                                            var donate = Math.round(Number(d.amount) * smulti);
 
                                             totalDonate += donate
 
