509c1ab10d87c835cdabc337c55f1795549a0842
Mo <MoMentalochka@yandex.ru>
2022-08-13 10:18:24 +0300

    fix mobile

--- components/statistic/index.js
+++ components/statistic/index.js
@@ -1,10 +1,10 @@
-var statistic = (function(){
+var statistic = (function () {
 
   var self = new nModule();
 
   var essenses = {};
 
-  var Essense = function(p){
+  var Essense = function (p) {
 
     var primary = deep(p, 'history');
 
@@ -17,50 +17,46 @@ var statistic = (function(){
     var fields = []
 
     var actions = {
-        getStat: async function() {
+      getStat: async function () {
 
-          let novblock = 1766504
+        // let novblock = 1766504
 
-          let block = await self.app.api.fetch('ping', {}, {timeout : 4000})
+        let block = await self.app.api.fetch('ping', {}, {timeout: 4000})
 
-          let from = (period?.from?.block && period?.from?.block > 0) ? period.from.block : block.height - novblock
+        let from = (period?.from?.block && period?.from?.block > 0) ? block.height - period.from.block : 0
+        let to = (period?.to?.block && (block.height - period.to.block)> 0) ? period.to.block : 0
+        fields = await self.app.api.rpc('getuserstatistic', [self.user.address.value, to, from, from,  1])
+        renders.block()
+      },
 
-          let to = (period?.to?.block && period?.to?.block > 0) ? period.to.block : 0
-
-          fields = await self.app.api.rpc('getuserstatistic', [self.user.address.value, to, from, from, 1] )
-          renders.block()
-        },
-
-        from: function (e) {
-          period.from.date = e.target.value
-          period.from.block = Math.floor((moment().unix() - moment(e.target.value).unix()) / 60)
-          renders.form()
-        },
-        to: function (e) {
-          period.to.date = e.target.value
-          period.to.block = Math.floor((moment().unix() - moment(e.target.value).unix()) / 60)
-          renders.form()
-        }
+      from: function (e) {
+        period.from.date = e.target.value
+        period.from.block = Math.floor((moment().unix() - moment(e.target.value).unix()) / 60)
+        renders.form()
+      },
+      to: function (e) {
+        period.to.date = e.target.value
+        period.to.block = Math.floor((moment().unix() - moment(e.target.value).unix()) / 60)
+        renders.form()
+      }
     }
 
-    var events = {
-
-    }
+    var events = {}
 
     var renders = {
       form: function (clbk) {
         self.shell({
 
-          name :  'form',
-          el :   el.form,
-          data : {
+          name: 'form',
+          el: el.form,
+          data: {
             period: period,
           },
-        }, function(_p) {
+        }, function (_p) {
           _p.el.find('.button').on('click', (e) => {
             e.preventDefault()
             actions.getStat()
-          } );
+          });
 
           _p.el.find('.from').on('change', actions.from)
           _p.el.find('.to').on('change', actions.to)
@@ -70,48 +66,48 @@ var statistic = (function(){
       block: function (clbk) {
         self.shell({
 
-          name :  'block',
-          el :   el.block,
-          data : {
+          name: 'block',
+          el: el.block,
+          data: {
             fields: fields,
           },
-        }, function(_p) {
+        }, function (_p) {
 
         })
       }
     }
 
     var state = {
-      save : function(){
+      save: function () {
 
       },
-      load : function(){
+      load: function () {
 
       }
     }
 
-    var initEvents = function(){
+    var initEvents = function () {
 
     }
 
     return {
-      primary : primary,
+      primary: primary,
 
-      getdata : async function(clbk){
+      getdata: async function (clbk) {
 
         var data = {
           period: period
         };
 
-            clbk(data);
+        clbk(data);
 
       },
 
-      destroy : function(){
+      destroy: function () {
         el = {};
       },
 
-      init : function(p){
+      init: async  function (p) {
 
         state.load();
 
@@ -122,6 +118,8 @@ var statistic = (function(){
         el.block = p.el.find('.block');
         el.form = p.el.find('.form')
 
+        period.to.block = 0
+        period.from.block = Math.floor((moment().unix() - moment('2022-07-01').unix()) / 60)
         renders.form()
 
         initEvents();
@@ -133,8 +131,7 @@ var statistic = (function(){
   };
 
 
-
-  self.run = function(p){
+  self.run = function (p) {
 
     var essense = self.addEssense(essenses, Essense, p);
 
@@ -142,9 +139,9 @@ var statistic = (function(){
 
   };
 
-  self.stop = function(){
+  self.stop = function () {
 
-    _.each(essenses, function(essense){
+    _.each(essenses, function (essense) {
 
       essense.destroy();
 
@@ -156,11 +153,9 @@ var statistic = (function(){
 })();
 
 
-if(typeof module != "undefined")
-{
+if (typeof module != "undefined") {
   module.exports = statistic;
-}
-else{
+} else {
 
   app.modules.statistic = {};
   app.modules.statistic.module = statistic;
