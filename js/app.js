2b91653b39224f02a150a776ae474b726494ce21
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-08-24 14:10:54 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2442,7 +2442,7 @@ Application = function(p)
       needmanage : false,
       hasupdate : false,
 
-      playstore : false,  ///// TODO
+      playstore : true,  ///// TODO
 
       downloadAndInstall : function(){
 
