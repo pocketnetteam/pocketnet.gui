4dfb84600f5dd54ae9fd522e250f74fffd29967d
Shpingalet007 <pavelvernigorov@gmail.com>
2022-08-31 12:17:31 +0200

    Some changes to build configs
    - Gulpfile tasks names provided.
    - Delegating styles building from Minimize.js to Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -118,9 +118,12 @@ exports.buildStyles.displayName = 'build:styles';
 /**
  * Task to clean CSS files.
  */
+const cleanComponentCss = () => gulp.src('./components/**/*.css', { allowEmpty: true }).pipe(clean());
+const cleanComponentCssMaps = () => gulp.src('./components/**/*.map', { allowEmpty: true }).pipe(clean());
+
 const cleanComponentStyles = gulp.series([
-    () => gulp.src('./components/**/*.css', { allowEmpty: true }).pipe(clean()),
-    () => gulp.src('./components/**/*.map', { allowEmpty: true }).pipe(clean()),
+    cleanComponentCss,
+    cleanComponentCssMaps,
 ]);
 const cleanMasterStyle = () => purgeCssFiles('./css/master.css');
 const cleanCssStyles = () => purgeCssFiles('./css/*.less');
