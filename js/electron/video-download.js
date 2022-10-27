c45815a80a44b6526169cade9ec58639eea9b69b
Shpingalet007 <22795961+shpingalet007@users.noreply.github.com>
2022-10-27 13:51:13 +0200

    Refactor code from df340a6d10d0e2e99fa198af83ea73fcc93d9380

--- js/electron/video-download.js
+++ js/electron/video-download.js
@@ -7,6 +7,13 @@ module.exports = class VideoDownload {
   PostsDir = 'posts';
   VideosDir = 'videos';
 
+  Regexps = {
+    Streams: /^.+\.m3u8/gm,
+    VideoTargetName: /#EXT-X-MAP:URI="(.+\.mp4)"/m,
+    BytesRange: /(?!BYTERANGE)(\d+@\d+)/gm,
+    FragmentNameRange: /(?!fragment_)(\d+)-(\d+)/,
+  };
+
   constructor(appPath) {
     this.Storage = appPath;
   }
@@ -37,6 +44,10 @@ module.exports = class VideoDownload {
     ipcMain.handle('getVideoData', async (event, shareId, videoId) => {
       return videoDownloader.getVideoData(shareId, videoId);
     });
+
+    ipcMain.handle('getVideoSegment', async (event, videoDir, filename) => {
+      return videoDownloader.getVideoSegment(videoDir, filename);
+    });
   }
 
   static detachHandlers(ipcMain) {
@@ -46,6 +57,7 @@ module.exports = class VideoDownload {
     ipcMain.removeHandler('getShareList');
     ipcMain.removeHandler('getShareData');
     ipcMain.removeHandler('getVideoData');
+    ipcMain.removeHandler('getVideoSegment');
   }
 
   async savePostData(shareData) {
@@ -66,10 +78,6 @@ module.exports = class VideoDownload {
     const signsDir = path.join(videoDir, 'signatures.json');
     const playlistDir = path.join(videoDir, 'playlist.m3u8');
 
-    const streamsRegexp = /^.+\.m3u8/gm;
-    const videoTargetFile = /#EXT-X-MAP:URI="(.+\.mp4)"/m;
-    const bytesRangesSelect = /(?!BYTERANGE)(\d+@\d+)/gm;
-
     const jsonData = JSON.stringify(videoData);
     fs.writeFileSync(jsonDir, jsonData, { overwrite: false });
 
@@ -80,7 +88,7 @@ module.exports = class VideoDownload {
     await this.downloadFile(signsUrl, { stream: signsFile });
 
     const streamsData = await this.downloadFile(playlistUrl);
-    const streamsList = streamsData.match(streamsRegexp);
+    const streamsList = streamsData.match(this.Regexps.Streams);
 
     const targetStream = streamsList.find((stream) => (
       stream.endsWith(`${videoResolution}.m3u8`)
@@ -92,9 +100,9 @@ module.exports = class VideoDownload {
 
     const fragmentsFile = fs.createWriteStream(playlistDir);
     const fragmentsData = await this.downloadFile(targetStreamUrl, { stream: fragmentsFile });
-    const fragmentsList = fragmentsData.match(bytesRangesSelect);
+    const fragmentsList = fragmentsData.match(this.Regexps.BytesRange);
 
-    const targetVideo = fragmentsData.match(videoTargetFile)[1];
+    const targetVideo = fragmentsData.match(this.Regexps.VideoTargetName)[1];
 
     const targetVideoUrl = `${targetStreamBaseUrl}/${targetVideo}`;
 
@@ -193,12 +201,96 @@ module.exports = class VideoDownload {
 
     const fileStats = fs.statSync(playlistPath);
 
+    const masterSwarmId = details.streamingPlaylists[0].playlistUrl;
+
+    const vurl = playlistPath.match(this.Regexps.VideoTargetName)[1];
+
+    let signatures;
+
+    try {
+      signatures = JSON.parse(fs.readFileSync(path.join(videoDir, 'signatures.json'), { encoding:'utf8', flag:'r' }));
+    } catch(e) {
+      console.log(e);
+    }
+
+    if (masterSwarmId && vurl && signatures) {
+      const url = masterSwarmId.split("/hls/")[0] + '/hls/' + details.uuid + '/' + vurl;
+
+      let i = -1;
+      let f = -1;
+
+      const fss = signatures.find((a, index) => {
+        f++;
+
+        if (index == vurl) {
+          return true;
+        }
+      });
+
+      if (fss) {
+        i = f;
+      }
+
+      if (i > -1) {
+        const segmentsFiles = _.sortBy(_.filter(videosList, (vl) => {
+          if (vl.endsWith('.mp4')) {
+            return true;
+          }
+        }), (vl => (
+          vl.match(this.Regexps.FragmentNameRange)[0]
+        )))
+
+        const segments = segmentsFiles.map((vl, j) => {
+          const j1 = j - 1;
+
+          const segmentRangeData = this.Regexps.FragmentNameRange;
+          const segmentRangeStr = `${segmentRangeData[1]}-${segmentRangeData[2]}`;
+
+          return {
+            sequence: `${j1}`,
+            range: segmentRangeStr,
+            priority: 1,
+            downloadBandwidth: 10,
+            streamId: `V${i}`,
+            masterManifestUri: masterSwarmId,
+            id: `${masterSwarmId}+V${i}+${j1}`,
+            requestUrl: url,
+            responseUrl: url,
+            masterSwarmId,
+            url,
+          };
+        });
+
+        const map = new Map();
+
+        segments.forEach((s) => {
+          map.set(s.id, { segment: s });
+        });
+
+        videoData.infos.segments = map;
+        videoData.infos.masterSwarmId = masterSwarmId;
+        videoData.infos.streamSwarmId = `${masterSwarmId}+V${i}`;
+        videoData.infos.dir = videoDir;
+        videoData.infos.trackerUrls = details.trackerUrls;
+      }
+    }
+
     videoData.size = fileStats.size;
     videoData.video = {
       internalURL: shareId,
     };
 
     return videoData;
+  }
+
+  getVideoSegment(videoDir, filename) {
+    try {
+      const data = fs.readFileSync(path.join(videoDir, filename), {  flag:'r' });
+
+      return data;
+    } catch(err) {
+      return null;
+    }
   }
 
   downloadFile(url, options = {}) {
