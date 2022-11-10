55cb2166f3b09d6e042c6745fecc8e736759651c
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-11-10 11:36:09 +0300

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -3,6 +3,7 @@ Actions = function(app){
      
     var key = 'actions_v0'
     var accounts = {}
+    var events = {}
     
     var exports = function(){
         return {}
@@ -11,7 +12,31 @@ Actions = function(app){
     var imports = function(value){
         accounts = value
 
-        
+        emit('change', accounts)
+    }
+
+    var emit = function(key, data){
+        _.each(events[key] || [], function(e){
+            e({data : data})
+        })
+    }
+
+    self.on = function(key, f){
+        if(!events[key]){
+            events[key] = []
+        }
+
+        events[key].push(f)
+    }
+
+    self.off = function(key, f){
+        if(!events[key]){
+            events[key] = []
+        }
+
+        events[key] = _.filter(events[key], function(k){
+            return k != f
+        })
     }
 
     self.save = function(){
@@ -40,7 +65,8 @@ Actions = function(app){
     }
 
     self.destroy = function(){
-
+        events = {}
+        accounts = {}
     }
     
     return self
