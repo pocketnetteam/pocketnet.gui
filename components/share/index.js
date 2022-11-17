54dd10026f8b931e2e2068447f21cfc7f60de8bc
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-11-17 15:06:59 +0300

    v

--- components/share/index.js
+++ components/share/index.js
@@ -975,9 +975,7 @@ var share = (function(){
 													self.app.Logger.error({
 														err: 'DAMAGED_LOCAL_STORAGE',
 														code: 801,
-														payload: {
-															...error,
-														},
+														payload: error,
 													  });
 												};
 
