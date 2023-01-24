MoMentalochka/pocketnet.gui-my

multiblock limit to 100

localization/en.js
@@ -261,7 +261,7 @@ ____loclib.lloadprev = "Load New Awesome Posts";
 
 
 // blocking
-____loclib.blockinglimit_100 = "You cannot select more than 10 users at a time"
+____loclib.blockinglimit_100 = "You cannot select more than 100 users at a time"
 ____loclib.blockingdisclaimer = "Do you want to also block connected accounts? ONLY do this for suspected bots."
 ____loclib.blockingsuccess = "The blocking was successful"
 ____loclib.blockinginprogress = "Multiple blocking is currently in progress, try again later"

localization/ru.js
@@ -267,7 +267,7 @@ ____loclib.lloadprev = "Загрузить свежие материалы"
 
 
 //blocking
-____loclib.blockinglimit_100 = "Нельзя выбрать более 10 пользователей за раз"
+____loclib.blockinglimit_100 = "Нельзя выбрать более 100 пользователей за раз"
 ____loclib.blockingdisclaimer = "Вы хотите также заблокировать связанные учетные записи? Делайте это только для подозреваемых ботов."
 ____loclib.blockingsuccess = "Блокировка прошла успешно"
 ____loclib.blockinginprogress = "В данный момент выполняется множественная блокировка, попробуйте позже"

pocketnetteam/pocketnet.gui

localization/en.js
@@ -305,7 +305,7 @@ _l.lloadprev = "Load New Awesome Posts";
 
 
 //blocking
-_l.blockinglimit_100 = "You cannot select more than 10 users at a time"
+_l.blockinglimit_100 = "You cannot select more than 100 users at a time"
 _l.blockingdisclaimer = "Do you want to block linked accounts as well? Do this only for suspected bots."
 _l.blockingsuccess = "The blocking was successful"
 _l.blockinginprogress = "Multiple blocking is currently in progress, try again later"

localization/ru.js
@@ -306,7 +306,7 @@ _l.lloadprev = "Загрузить свежие материалы"
 
 
 //blocking
-_l.blockinglimit_100 = "Нельзя выбрать более 10 пользователей за раз"
+_l.blockinglimit_100 = "Нельзя выбрать более 100 пользователей за раз"
 _l.blockingdisclaimer = "Вы хотите также заблокировать связанные учетные записи? Делайте это только для подозреваемых ботов."
 _l.blockingsuccess = "Блокировка прошла успешно"
 _l.blockinginprogress = "В данный момент выполняется множественная блокировка, попробуйте позже"