361df28a679eee88285ff19ad1772462e036e7d7
j-bitmaker <nikolyaegorytch@yandex.ru>
2023-02-02 15:30:18 +0300

    unableToAuthorizeBody

--- localization/en.js
+++ localization/en.js
@@ -1337,7 +1337,7 @@ _l.sortByDuration = "Duration";
 _l.sortByViews = "Views";
 
 _l.unableToAuthorize = "Unable to authorize";
-_l.unableToAuthorizeBody = "Unfortunately, the application cannot authenticate this account on the video server. You need at least 5 PKOIN or 100 reputation to upload videos.";
+_l.unableToAuthorizeBody = "Unfortunately, you cannot upload videos to Bastyon yet. You need at least 5 PKOIN or 100 reputation to upload videos. Now you can publish videos from YouTube, just paste the link into your post.";
 
 _l.unableToAuthorizeConnection = "Unable to authorize";
 _l.unableToAuthorizeConnectionBody = "Unfortunately, the application cannot authenticate this account on the video server.  Please try again later";
