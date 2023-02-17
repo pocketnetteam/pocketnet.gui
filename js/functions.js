ea8d077ea6567f7f60d41bc614cba565bd4eec56
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2023-02-17 17:44:59 +0300

    v

--- js/functions.js
+++ js/functions.js
@@ -12358,96 +12358,100 @@ var connectionSpeed = function()
 
 /*test*/
 /*
-Window.prototype._addEventListener = Window.prototype.addEventListener;
+if(typeof Window != 'undefined'){
 
-Window.prototype.addEventListener = function(a, b, c) {
-   if (c==undefined) c=false;
-   this._addEventListener(a,b,c);
-   if (! this.eventListenerList) this.eventListenerList = {};
-   if (! this.eventListenerList[a]) this.eventListenerList[a] = [];
-   this.eventListenerList[a].push({listener:b,options:c});
-};
+	Window.prototype._addEventListener = Window.prototype.addEventListener;
 
-EventTarget.prototype._addEventListener = EventTarget.prototype.addEventListener;
-
-EventTarget.prototype.addEventListener = function(a, b, c) {
-   if (c==undefined) c=false;
-   this._addEventListener(a,b,c);
-   if (! this.eventListenerList) this.eventListenerList = {};
-   if (! this.eventListenerList[a]) this.eventListenerList[a] = [];
-   this.eventListenerList[a].push({listener:b,options:c});
-};
-
-EventTarget.prototype._getEventListeners = function(a) {
+	Window.prototype.addEventListener = function(a, b, c) {
+	if (c==undefined) c=false;
+	this._addEventListener(a,b,c);
 	if (! this.eventListenerList) this.eventListenerList = {};
-	if (a==undefined)  { return this.eventListenerList; }
-	return this.eventListenerList[a];
- };
+	if (! this.eventListenerList[a]) this.eventListenerList[a] = [];
+	this.eventListenerList[a].push({listener:b,options:c});
+	};
 
- EventTarget.prototype._removeEventListener = EventTarget.prototype.removeEventListener;
-EventTarget.prototype.removeEventListener = function(a, b ,c) {
-   if (c==undefined) c=false;
-   this._removeEventListener(a,b,c);
-   if (! this.eventListenerList) this.eventListenerList = {};
-   if (! this.eventListenerList[a]) this.eventListenerList[a] = [];
+	EventTarget.prototype._addEventListener = EventTarget.prototype.addEventListener;
 
-   for(let i=0; i < this.eventListenerList[a].length; i++){
-      if(this.eventListenerList[a][i].listener==b, this.eventListenerList[a][i].options==c){
-          this.eventListenerList[a].splice(i, 1);
-          break;
-      }
-   }
-   if(this.eventListenerList[a].length==0) delete this.eventListenerList[a];
-};
+	EventTarget.prototype.addEventListener = function(a, b, c) {
+	if (c==undefined) c=false;
+	this._addEventListener(a,b,c);
+	if (! this.eventListenerList) this.eventListenerList = {};
+	if (! this.eventListenerList[a]) this.eventListenerList[a] = [];
+	this.eventListenerList[a].push({listener:b,options:c});
+	};
 
+	EventTarget.prototype._getEventListeners = function(a) {
+		if (! this.eventListenerList) this.eventListenerList = {};
+		if (a==undefined)  { return this.eventListenerList; }
+		return this.eventListenerList[a];
+	};
 
- function listAllEventListeners() {
-	const allElements = Array.prototype.slice.call(document.querySelectorAll('*'));
-	allElements.push(document);
-	allElements.push(window);
-  
-	const types = [];
-  
-	for (let ev in window) {
-	 if (/^on/.test(ev)) types[types.length] = ev;
-	}
-  
-	let elements = [];
-	for (let i = 0; i < allElements.length; i++) {
-	  const currentElement = allElements[i];
-  
-	  // Events defined in attributes
-	  for (let j = 0; j < types.length; j++) {
-  
-		if (typeof currentElement[types[j]] === 'function' ) {
-		  elements.push({
-			"node": currentElement,
-			"type": types[j],
-			"func": currentElement[types[j]].toString(),
-		  });
+	EventTarget.prototype._removeEventListener = EventTarget.prototype.removeEventListener;
+	EventTarget.prototype.removeEventListener = function(a, b ,c) {
+	if (c==undefined) c=false;
+	this._removeEventListener(a,b,c);
+	if (! this.eventListenerList) this.eventListenerList = {};
+	if (! this.eventListenerList[a]) this.eventListenerList[a] = [];
+
+	for(let i=0; i < this.eventListenerList[a].length; i++){
+		if(this.eventListenerList[a][i].listener==b, this.eventListenerList[a][i].options==c){
+			this.eventListenerList[a].splice(i, 1);
+			break;
 		}
-	  }
-  
-	  // Events defined with addEventListener
-	  if (typeof currentElement._getEventListeners === 'function') {
-		evts = currentElement._getEventListeners();
-		if (Object.keys(evts).length >0) {
-		  for (let evt of Object.keys(evts)) {
-			for (k=0; k < evts[evt].length; k++) {
+	}
+	if(this.eventListenerList[a].length==0) delete this.eventListenerList[a];
+	};
 
-				if(evts[evt][k].listener){
-					elements.push({
-						"node": currentElement,
-						"type": evt,
-						"func": evts[evt][k].listener.toString()
-					  });
-				}
-			  
+
+	function listAllEventListeners() {
+		const allElements = Array.prototype.slice.call(document.querySelectorAll('*'));
+		allElements.push(document);
+		allElements.push(window);
+	
+		const types = [];
+	
+		for (let ev in window) {
+		if (/^on/.test(ev)) types[types.length] = ev;
+		}
+	
+		let elements = [];
+		for (let i = 0; i < allElements.length; i++) {
+		const currentElement = allElements[i];
+	
+		// Events defined in attributes
+		for (let j = 0; j < types.length; j++) {
+	
+			if (typeof currentElement[types[j]] === 'function' ) {
+			elements.push({
+				"node": currentElement,
+				"type": types[j],
+				"func": currentElement[types[j]].toString(),
+			});
 			}
-		  }
 		}
-	  }
+	
+		// Events defined with addEventListener
+		if (typeof currentElement._getEventListeners === 'function') {
+			evts = currentElement._getEventListeners();
+			if (Object.keys(evts).length >0) {
+			for (let evt of Object.keys(evts)) {
+				for (k=0; k < evts[evt].length; k++) {
+
+					if(evts[evt][k].listener){
+						elements.push({
+							"node": currentElement,
+							"type": evt,
+							"func": evts[evt][k].listener.toString()
+						});
+					}
+				
+				}
+			}
+			}
+		}
+		}
+	
+		return elements.sort();
 	}
-  
-	return elements.sort();
-  }*/
+
+}*/
