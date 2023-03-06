8fdb618e06e471f5cf530473451de26bed58fc11
AlexandrHat <71383529+AlexandrHat@users.noreply.github.com>
2023-03-06 12:28:59 +0300

    how to buy

--- components/howtobuy/index.js
+++ components/howtobuy/index.js
@@ -8,7 +8,7 @@ var howtobuy = (function(){
 
 		var primary = deep(p, 'history');
 
-		var el, ed;
+		var el, ed, ext = null;
 
 		var actions = {
 
@@ -19,7 +19,64 @@ var howtobuy = (function(){
 		}
 
 		var renders = {
+			faq : function(){
 
+				var faqLangsFiltered = {};
+
+				var faqLangs = self.sdk.faqLangs.get() 
+
+				for (var l in faqLangs){
+
+					if (l !== 'fr'){
+
+
+						faqLangsFiltered[l] = faqLangs[l]
+						.filter(function(g){
+
+							return g.id === 'buy-pkoin';
+	
+						})
+						.map(function(g){
+							
+							if (g.id === 'buy-pkoin'){
+
+								var newG = JSON.parse(JSON.stringify(g))
+
+								newG.group = g.group.slice(0, 7)
+
+								return newG;
+							}
+
+							return g;
+							
+						})
+
+					}
+
+				}
+
+				
+				var k = self.app.localization.key;
+
+				if(!faqLangsFiltered[k]) k = 'en';
+
+				var faqcontent = faqLangsFiltered[k];
+
+
+				self.shell({
+					name :  'faq',
+					turi : 'easynode',
+					el : el.faqWrapper,
+					data : {
+						groups : faqcontent
+					},
+					animation : false,				
+
+				}, function(p){
+
+
+				})
+			}
 		}
 
 		var state = {
@@ -33,7 +90,21 @@ var howtobuy = (function(){
 
 		var initEvents = function(){
 			
+		
+			
 
+		}
+
+		var make = function(){
+			/*self.app.platform.papi.post(
+				"18399921cc5455b3283322a488f553d0169d00e3501246fc60d2e0c67c98bfc6",
+				el.lenta,
+				function (e, p) {
+					ext = p
+				},
+			);*/
+
+			renders.faq()
 		}
 
 		return {
@@ -52,6 +123,11 @@ var howtobuy = (function(){
 			},
 
 			destroy : function(){
+
+				if(ext){
+					ext.destroy()
+					ext = null
+				}
 				ed = {}
 				el = {};
 			},
@@ -63,7 +139,15 @@ var howtobuy = (function(){
 				el = {};
 				el.c = p.el.find('#' + self.map.id);
 
+				el.faqWrapper = el.c.find('.faqWrapper')
+				el.lenta = el.c.find('.lentaWrapper')
+				el.c.find('.click').on('click', function(){ 
+					el.c.find('.formula').toggleClass("hidden")
+				})
+
 				initEvents();
+
+				make()
 
 				p.clbk(null, p);
 			}
@@ -106,3 +190,4 @@ else{
 	app.modules.howtobuy.module = howtobuy;
 
 }
+
