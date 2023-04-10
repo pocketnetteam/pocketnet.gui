552e0b0ebaa56ea0ccf1269dc896a091d09be0f1
j-bitmaker <nikolyaegorytch@yandex.ru>
2023-04-10 11:17:57 +0300

    clean

--- components/earnings/index.js
+++ components/earnings/index.js
@@ -11,102 +11,29 @@ var earnings = (function () {
 
     var el;
 
-    var charts = {};
-
     var selectedPeriod = {
       from: {},
       to: {}
     }
 
-    var serversList = {};
-		var peertubeServers = {};
-
     var stats = [{
       balance : 13.4539127,
       id : "amountDonation",
       label : self.app.localization.e('amountDonation'),
-      move : {
-        neutral: {
-          summary : 0,
-          color : "#414244"
-        },
-        positive: {
-          summary : 0,
-          color : "#0F8623"
-        }
-      }
     },
     {
       balance : 13.4539127,
       id : "amountLottery",
       label : self.app.localization.e('amountLottery'),
-      move : {
-        neutral: {
-          summary : 0,
-          color : "#414244"
-        },
-        positive: {
-          summary : 0,
-          color : "#0F8623"
-        }
-      }
     },
     {
       balance : 13.4539127,
       id : "amountTransfer",
       label : self.app.localization.e('amountTransfer'),
-      move : {
-        neutral: {
-          summary : 0,
-          color : "#414244"
-        },
-        positive: {
-          summary : 0,
-          color : "#0F8623"
-        }
-      }
     }]
 
 
-    var lastMonth = moment().subtract(1, 'months').format('YYYY-MM-DD'); 
-    var fields
-
     var loading = false
-
-    var prevPeriod;
-
-    var helpers = {
-			removeDuplicateVideos(host, videos) {
-				let formattingVideos = [...videos];
-
-				const serverRoy = Object.keys(serversList).find((royKey) =>
-					(serversList[royKey] || []).includes(host),
-				);
-
-				(serversList[serverRoy] || []).forEach((server) => {
-					if (server === host) return;
-
-					if (!peertubeServers[server]) return;
-
-					formattingVideos = formattingVideos.filter((video) => {
-						const duplicate = peertubeServers[server].videos.find(
-							(duplicatedVideo) => video.uuid === duplicatedVideo.uuid,
-						);
-
-						if (duplicate) {
-							//pick max amount of views
-							duplicate.views = Math.max(duplicate.views, video.views);
-
-							return false;
-						}
-
-						return true;
-					});
-				});
-
-				return formattingVideos;
-			}
-		}
 
     var actions = {
 
@@ -137,18 +64,6 @@ var earnings = (function () {
                 if (stat){
                   
                   stat.balance = statValues[key] / 100000000;
-
-                  if (stat.balance){
-
-                    stat.move.positive.summary = stat.balance;
-                    stat.move.neutral.summary = 0;
-
-                  } else {
-
-                    stat.move.positive.summary = 0;
-                    stat.move.neutral.summary = 1;
-
-                  }
 
                   renders.total(stat);
 
