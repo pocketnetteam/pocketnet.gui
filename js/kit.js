6d987724275471271b6ac5caca98616abf815cb5
Shpingalet007 <22795961+shpingalet007@users.noreply.github.com>
2023-05-18 18:59:04 +0200

    Add check functions
    * Check for embed video function
    * Check for ipfs video function

--- js/kit.js
+++ js/kit.js
@@ -1492,6 +1492,32 @@ Share = function(lang){
 		if(meta.type == 'peertube' && ch && ch.length > 0 && ch[ch.length - 1] == 'audio') return true
 	}
 
+	self.itisembed = function(){
+		if (self.settings.v === 'a' || !self.url?.v) {
+			return;
+		}
+
+		const meta = parseVideo(self.url.v);
+
+		const isYoutube = (meta.type === 'youtube');
+		const isVimeo = (meta.type === 'vimeo');
+		const isBitchute = (meta.type === 'bitchute');
+		const isBrighteon = (meta.type === 'brighteon' || meta.type === 'stream.brighteon');
+		const isIpfs = (meta.type === 'ipfs');
+
+		return (isYoutube || isVimeo || isBitchute || isBrighteon || isIpfs);
+	}
+
+	self.itisipfs = function(){
+		if (self.settings.v === 'a' || !self.url?.v) {
+			return;
+		}
+
+		const meta = parseVideo(self.url.v);
+
+		return (meta.type === 'ipfs');
+	}
+
 	self.itisstream = function(){
 
 		if(self.settings.v == 'a') return
@@ -2307,6 +2333,32 @@ pShare = function(){
 		var ch = self.url.replace('peertube://', '').split('/')
 
 		if(meta.type == 'peertube' && ch && ch.length > 0 && ch[ch.length - 1] == 'audio') return true
+	}
+
+	self.itisembed = function(){
+		if (self.settings.v === 'a' || !self.url?.v) {
+			return;
+		}
+
+		const meta = parseVideo(self.url.v);
+
+		const isYoutube = (meta.type === 'youtube');
+		const isVimeo = (meta.type === 'vimeo');
+		const isBitchute = (meta.type === 'bitchute');
+		const isBrighteon = (meta.type === 'brighteon' || meta.type === 'stream.brighteon');
+		const isIpfs = (meta.type === 'ipfs');
+
+		return (isYoutube || isVimeo || isBitchute || isBrighteon || isIpfs);
+	}
+
+	self.itisipfs = function(){
+		if (self.settings.v === 'a' || !self.url?.v) {
+			return;
+		}
+
+		const meta = parseVideo(self.url.v);
+
+		return (meta.type === 'ipfs');
 	}
 
 	self.itisstream = function(){
