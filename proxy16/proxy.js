fbc6bbfc6c93add0212f4ae6ab73dda57b56b6e3
zanhesl <48605213+zanhesl@users.noreply.github.com>
2023-05-22 19:04:11 +0300

    patch: peertube servers list (#1046)
    * Servers group 10 is archived
    * Servers group 12 is archived
    * Servers group 36 is archived

--- proxy16/proxy.js
+++ proxy16/proxy.js
@@ -762,12 +762,14 @@ var Proxy = function (settings, manage, test, logger, reverseproxy) {
 				  {
 					host: 'pocketnetpeertube10.nohost.me',
 					cantuploading: true,
+					offline: true,					
 					ip: '23.254.226.253',
 				  },
 				  {
 					host: 'pocketnetpeertube11.nohost.me',
 					cantuploading: true,
 					ip: '84.252.138.108',
+					offline: true,					
 				  },
 				  { host: 'peertube.archive.pocketnet.app', cantuploading: true, ip: '178.217.159.221'},
 				],
@@ -777,12 +779,15 @@ var Proxy = function (settings, manage, test, logger, reverseproxy) {
 					host: 'bastyonmma.pocketnet.app',
 					cantuploading: true,
 					ip: '88.99.34.74',
+					offline: true,					
 				  },
 				  {
 					host: 'bastyonmma.nohost.me',
 					cantuploading: true,
 					ip: '49.12.231.72',
+					offline: true,
 				  },
+				  { host: 'peertube.archive.pocketnet.app', cantuploading: true, ip: '178.217.159.221'},
 				],
 
 				13: [
@@ -1042,7 +1047,9 @@ var Proxy = function (settings, manage, test, logger, reverseproxy) {
 						host : 'peertube31.pocketnet.app',
 						ip: '185.148.146.11',
 						special: true,
-					}
+						cantuploading: true,
+					},
+					{ host: 'peertube.archive.pocketnet.app', cantuploading: true, ip: '178.217.159.221'},
 				],
 
 				37: [
