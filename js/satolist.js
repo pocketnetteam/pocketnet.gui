77eba3f655044d67ca25d85e274c1c0a5597d29c
Shpingalet007 <22795961+shpingalet007@users.noreply.github.com>
2023-11-21 14:38:24 +0100

    Satolist base changes

--- js/satolist.js
+++ js/satolist.js
@@ -29,14 +29,8 @@ Platform = function (app, listofnodes) {
     self.lastblocktime = null
     self.lasttimecheck = null
 
-    /// STREAM
+    self.streamlib = null;
 
-    /*
-    
-    self.streamlib = null
-    
-    */
-    
     self.bwdictionary = _.map(["masturbation", "ussalovo", "usralovo", "porevo", "naebalovo", "podralovo", "B_HECO3HAHKE", "B_HE3HAHKE", "XEPOTA", "XEPOBOE", "ebanat", "uyebanets", "XEPOBOE", "XYEBOE", "pizdabol", "HOC_B_KAKEHE", "s_pekla_na_huy", "XEP_TE_B_HOC", "KAKAXA", "KAKA_HA_XEPE", "XEP_B_KAKE", "HOCOM_B_XEP", "HOCOM_B_KAKEH", "razblyadko", "puki_puki", "zablyadko", "eblanko", "perdelko", "ohuelko", "zalupko", "XYEET_B_XYETE", "pizdelko", "OXYETb", "uyeban", "ueban", "OXYETb_OHO", "XYETE", "XYEET", "XMbIPb", "XAPKOTA", "PEDRILLO", "MPA3b", "PIZDOTA", "XMbIPbKA", "MPA3OTA", "PIZDLO", "TBAPb", "POTOM_HACOC", "UKRAM_PIZDA", "UKROPETS", "PIZDLO", "PEDRILLO", "SSUKA", "3ABOHbKA", "3ABOHbKO", "3ACCAHbKO", "3ACCATKA", "3ACCATKO", "3ACEPbKA", "PIZDUK", "3ACCAHbKA", "3ACEPbKO", "3ACEPEH_MO3K", "3ACPATKO", "BOHbKO", "BOHbKA", "CCAHbKA", "CCAKA", "CCAKOTKA", "CCAHbKO", "CCAKOTKO", "CCbIKOTKA", "CKBEPHA", "CPAKOBMA3", "CPAKA", "CPAKATbIKA", "CPAKATbIKAH", "CPAKATbIKAHA", "CPAKOBMA3KO", "CPAKOTbIK", "CPAMEH", "CPAMOTA", "CPAKOCPAM", "CPAMHOMA3", "KPbICA", "PAKOM_CTAHb", "KAKAXOMA3KA", "PAKOMBCTAHb", "KAKAXOMA3", "PAKOMBCTAHbKA", "PBAHbE", "PBOTA", "PBOTHA_BOHbKA", "TbIX_DbIPA", "PIDOR", "PIDARAS", "PEDRILLON", "PEDRILKA", "PEDRILLION", "PEDRILKO", "PIDOROK", "PIDORKA", "PIDOR", "PIDORKO", "PIDARASKA", "PIDARASKO", "PIDERASKO", "PIDER", "PIDARASKO", "PERDUCCIO", "PERDILLO", "PERDILLION", "PERDILLIAN", "PERDILION", "PERDILO", "PERDILIAN", "PERDAK", "ELDAK", "PERDOLO", "PERDOLLO", "PERDOLLION", "PERDOLLIAN", "PERDOLIAN", "PERDOLION", "PERDULO", "PERDULLO", "PERDULLION", "PERDULLIAN", "PERDULKO", "PERDULION", "PERDULINA", "PERDULINO", "ZAPERDULINO", "PERDULIN", "ZAPERDULIN", "ZAPERDELANO", "ZAPERDELENO", "ZAPERDOLINO", "ELDACHINA", "MUDILLION", "MUDILA", "MUDILLO", "3ACPATKA", "PERDULIAN", "DURAK", "EUROUKR", "BCPATb", "3AMECbI", "CPATb", "BKAKATb", "3ACPATb", "BbICPATb", "HACPATb", "KAKATb", "BbIKAKATb", "HAKAKATb", "XYETb", "3AKAKATb", "XYEBOTEBO", "DEBILKA", "DEBILKO", "DEBILOK", "AXYEBATb", "XYEBOM", "XYEBOTEBO", "XYEBOTHOE", "XYEBOM", "XYETE", "zalupo", "zalupe", "zalupin", "zalupa", "zalupn", "PA3OCPATKA", "XYEK", "3ATbIKE", "KYKAPEKY", "poyebotevo", "KY_KA_PE_KY", "KYKAPEKOBO", "ebanadel", "ebanadelnyj", "B_POT_TE_CCbI", "B_POT_TE_CEPb", "chpok_chpok", "chpoki_chpoki", "PIZDOTOCHKA", "PIZDYONKA", "PIZDATOCHKA", "PIZDOTKA", "PIZDA", "MOXHATKA", "PILOTKA", "EBUCHKA", "TbIX_TbIXTbIX", "ZLOEBUCHKA", "ZLAJA_PIZDA", "PIZDISCHA", "PIZDENN", "UKROPIZDA", "3ABOHbKA", "CCbIKOTKA", "_3ABOHbKA", "3ACCAHbKO", "3ACEPbKA", "3ACPATKA", "POLONIZED", "BOHbKO", "3ACEPEH_MO3K", "EUROUKR", "MPA3b", "HUYLO", "EBLAN", "CPAKOTbIK", "CPAKOBMA3"], (s) => {return s.toLowerCase()})
 
     self.real = {
@@ -22662,50 +22656,43 @@ Platform = function (app, listofnodes) {
     
                         }, 2000)
     
+                        options.getPixi = (clbk) => {
+                            const appVersion = numfromreleasestring(window.packageversion);
+                            const appVersionSuffix = window.versionsuffix || '0';
+                            const vs = `${appVersion}_${appVersionSuffix}`;
                        
-                        /// STREAM
-
-                        /*
-
-                        options.getpixi = function(clbk){
-
-                            var vs = numfromreleasestring(window.packageversion) + '_' + (window.versionsuffix || "0")
-
-                            importScript('js/vendor/pixi.min.js?v=' + vs, clbk)
-                          
+                            importScript(`js/vendor/pixi.min.js?v=${vs}`, clbk);
                         }
 
-                        options.address = self.app.user.address.value
+                        options.address = self.app.user.address.value;
 
+                        // TODO SH007: Look further here...
                         if (self.streamlib) {
-                            self.streamlib.destroy()
+                            self.streamlib.destroy();
                         }
 
-                        self.streamlib = new Streamlib({
-                            options
+                        options.getPixi((PixiLib) => {
+                            const { BastyonStreams } = BastyonStreamsCreator(/* PIXI INSTANCE */);
+
+                            // self.streamlib = new BastyonStreams();
+
+                            // TODO SH007: Add stream verification function as second argument
+                            const userActiveStream = BastyonStreams.continueStreaming(options.address);
+
+                            if (userActiveStream) {
+                                new dialog('You have an active stream. Do you want to continue it?')
+                                    .continue(() => {
+                                        self.app.nav.api.load({
+                                            open : true,
+                                            href : 'video=1&v=', // TODO SH007: Add post TXID to href
+                                            history : true,
+                                        });
+                                    })
+                                    .no(() => {
+                                        // TODO SH007: Finish stream with Peertube API call
+                                    });
+                            }
                         })
-
-                        var activestream = streamlib.hasactiveStream()
-                        
-                        if (activestream){
-
-                            new dialog(you have stream. Continue stream or end).continue( () => {
-
-                                self.app.nav.api.load({
-                                    open : true,
-                                    href : 'video=1&v=' + activestream.txid,
-                                    history : true
-                                })
-
-                            }).no( () => {
-                                activestream.endStream()
-                            })
-
-                            
-                        }
-                        
-                        */
-    
                     })
                 })
                 
