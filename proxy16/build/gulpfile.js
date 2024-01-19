507f8e45cf4a12120b8885ffc875d35280e56421
Shpingalet007 <22795961+shpingalet007@users.noreply.github.com>
2024-01-19 13:49:54 +0100

    Separated build for Proxy16
    * Separate Proxy16 build
    * Using Gulp for Proxy16 build
    * Workflows update

---
+++ proxy16/build/gulpfile.js
@@ -0,0 +1,35 @@
+const { series } = require("gulp");
+
+require("better-logging")(console, {
+	format: ctx => {
+		return `${ctx.time.replace(/\.\d+/, '')} ${ctx.type} ${ctx.msg}`
+	}
+});
+
+const config = require("./config.json");
+
+const path = require("path");
+const { execSync } = require("child_process");
+
+// Configuration constants
+const PeertubeListLink = config.peertubesListLink;
+const PeertubeTargetFile = "../peertube-servers.json";
+
+function bundlePeertube(finished) {
+	const deployPath = path.join(__dirname, PeertubeTargetFile);
+
+	console.info("Gathering Peertube list from Github repository");
+
+	try {
+		execSync(`curl ${PeertubeListLink} --output ${deployPath} -s`, {
+			encoding: "utf8",
+			windowsHide: true,
+		});
+	} catch (e) {
+		console.error("Repository is not available!");
+	}
+
+	finished();
+}
+
+exports.build = series([bundlePeertube]);
