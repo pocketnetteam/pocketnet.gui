861264a115e66c94f1c3e477b58102e647cfc9e8
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2024-03-15 10:48:10 +0300

    v

--- proxy16/cli.js
+++ proxy16/cli.js
@@ -1,7 +1,7 @@
 global.MIN_NODES_COUNT = 10
 global.WRITE_LOGS = true
 global.USE_TRUST_NODES_ONLY = false
-global.USE_TLS_NODES_ONLY = true
+global.USE_TLS_NODES_ONLY = false
 
 
 var kit = require('./kit.js');
