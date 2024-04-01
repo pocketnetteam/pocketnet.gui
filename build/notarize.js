54b1cef3b9710473be174954995f820184d18f51
Pavel Vernigorov <22795961+shpingalet007@users.noreply.github.com>
2024-04-01 14:14:44 +0200

    Update notarize.js
    Use notarytool

--- build/notarize.js
+++ build/notarize.js
@@ -15,6 +15,7 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
+    tool: "notarytool",
     appBundleId: 'app.pocketnet.gui',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
