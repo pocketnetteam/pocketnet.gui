a6313889994d2e281ba0f501754ab6aff4bc0b95
Pavel Vernigorov <22795961+shpingalet007@users.noreply.github.com>
2024-04-02 11:59:00 +0200

    Update notarycheck.js

--- notarycheck.js
+++ notarycheck.js
@@ -1,25 +1,19 @@
-const core = require('@actions/core');
 const { spawn } = require('child_process');
 
 function isNotaryToolAvailable() {
-    return new Promise((resolve) => {
     const command = spawn('xcrun', ['--find', 'notarytool']);
 
     command.on('exit', function (code, signal) {
-        core.info(`EXIT: ${code} ${signal}`);
-        resolve(true);
+        console.log(`EXIT: ${code} ${signal}`);
     });
 
     command.stdout.on('data', (data) => {
-        core.info(`STDOUT:\n${data}`);
+        console.log(`STDOUT:\n${data}`);
     });
     
     command.stderr.on('data', (data) => {
-        core.info(`STDERR:\n${data}`);
-    });
+        console.log(`STDERR:\n${data}`);
     });
 }
 
-(async () => {
-    await isNotaryToolAvailable();
-})();
+isNotaryToolAvailable();
