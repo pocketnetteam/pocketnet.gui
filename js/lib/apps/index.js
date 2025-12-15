35a229f7b427505b1ac26696efee618be0568598
Voyager-3rd <167764214+Voyager-3rd@users.noreply.github.com>
2025-12-15 13:29:58 +0300

    change transactions api version to 7 (#1576)

--- js/lib/apps/index.js
+++ js/lib/apps/index.js
@@ -823,7 +823,7 @@ var BastyonApps = function (app) {
                     margintop: document.documentElement.style.getPropertyValue('--app-margin-top') || document.documentElement.style.getPropertyValue('--app-margin-top-default') || '0px',
                     application: application.manifest,
                     project: project_config,
-                    transactionsApiVersion: 6,
+                    transactionsApiVersion: 7,
                     alttransport : app.hasTor || false
                 })
             }
