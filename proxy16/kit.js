6dbb766b8fd655ece9eab4f0fe73cb784b65d0fa
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2026-06-02 19:42:21 +0300

    v

--- proxy16/kit.js
+++ proxy16/kit.js
@@ -1436,6 +1436,13 @@ const kit = {
 
 		settings = state.expand(environmentDefaultSettings, settings)
 
+
+		if(typeof global.usecli != undefined && global.usecli){
+			if (settings.tor && settings.tor.enabled3){
+				settings.tor.enabled3 = 'neveruse'
+			}
+		}
+
 		db = new Datastore({
 			filename: f.path(settingsPath),
 		});
